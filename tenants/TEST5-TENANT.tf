e427256ddfc96b51342c5fadb0d46125343b608e
Kevin Corbin <kecorbin@users.noreply.github.com>
2020-02-11 14:59:49 -0600

    Update TEST5-TENANT.tf

--- tenants/TEST5-TENANT.tf
+++ tenants/TEST5-TENANT.tf
@@ -3,7 +3,7 @@ resource "aci_tenant" "TEST5-tenant" {
   description = "This tenant is created by terraform"
 }
 
-resource "aci_application_profile" "test-app" {
+resource "aci_application_profile" "test5-app" {
   tenant_dn   = "${aci_tenant.TEST5-tenant.id}"
   name        = "TEST5-app1"
   description = "This app profile is created by terraform"
